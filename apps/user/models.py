73198e06b72e09ee25199e513962d338c52f310d
Pill <zhaopengtaochn@gmail.com>
2019-05-22 00:44:47 +0800

    goods & user models edit

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,40 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from db.base_model import BaseModel
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser, BaseModel):
+    """用户模型类"""
+
+    class Meta():
+        db_table = 'df_user'
+        verbose_name = '用户'
+
+
+'''
+    def generate_active_token(self):
+        """生成用户签名字符串"""
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        info = {
+            'confirm': self.id,
+        }
+        token = serializer.dump(info)
+        # return token.decode()
+'''
+
+
+class Address(BaseModel):
+    user = models.ForeignKey('User', verbose_name='所属账户')
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, null=True, verbose_name='邮政编码')
+    phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+
+    class Meta():
+        db_table = 'df_address'
+        verbose_name = '地址'
