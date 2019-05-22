90316b72bf59dc4046f3def70820d686ada907d7
Pill <zhaopengtaochn@gmail.com>
2019-05-22 16:47:01 +0800

    remove Meta brackets

--- db/base_model.py
+++ db/base_model.py
@@ -7,6 +7,6 @@ class BaseModel(models.Model):
     update_time = models.DateTimeField(auto_now_add=True, verbose_name='更新时间')
     is_deleted = models.BooleanField(default=False, verbose_name='删除标记')
 
-    class Meta():
+    class Meta:
         """说明BaseModel是一个抽象模型类"""
         abstract = True
