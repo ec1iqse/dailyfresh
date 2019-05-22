b2602e08d1f4eea81eea3480d6daa8e78800e2be
Pill <zhaopengtaochn@gmail.com>
2019-05-22 23:28:39 +0800

    Use Redis to solve THE cart

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 
 # Create your models here.
-class CartInfo(BaseModel):
-
-    class Meta:
-        verbose_name = '购物车商品信息'
-        verbose_name_plural = verbose_name
+# 使用redis实现购物车的功能
+#
+#
+# class CartInfo(BaseModel):
+#
+#     class Meta:
+#         verbose_name = '购物车商品信息'
+#         verbose_name_plural = verbose_name
