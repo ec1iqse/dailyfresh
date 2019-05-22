5374ff5e06d96aef26842582a7926cb99aad4bf9
Pill <zhaopengtaochn@gmail.com>
2019-05-22 23:21:52 +0800

    Still Not Fix The Problems

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,6 +1,10 @@
 from db.base_model import BaseModel
 from django.db import models
 
+
 # Create your models here.
 class CartInfo(BaseModel):
-    pass
+
+    class Meta:
+        verbose_name = '购物车商品信息'
+        verbose_name_plural = verbose_name
