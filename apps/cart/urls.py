735c6b3478faea8c6642ccbe74f1564770a4343a
Pill <zhaopengtaochn@gmail.com>
2019-09-22 20:10:44 +0800

    购物车页面显示 购物车记录更新 购物车增加 购物车记录删除

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from .views import CartAddView
 from .views import CartInfoView
 from .views import CartUpdateView
+from .views import CartDeleteView
 
 # app_name = 'cart'
 urlpatterns = [
     path('add', CartAddView.as_view(), name='add'),  # 购物车记录添加
     path('', CartInfoView.as_view(), name='show'),  # 购物车页面显示
     path('update', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
+    path('delete', CartDeleteView.as_view(), name='delete'),  # 购物车记录删除
 
 ]
